7d98835820d129e8374ef178cf2b325c5239f933
Ines <akrap.ines@gmail.com>
2019-09-03 15:00:37 +0200

    Add static files

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const client = new language.LanguageServiceClient();
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/static', express.static('static'));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/home.html'));
